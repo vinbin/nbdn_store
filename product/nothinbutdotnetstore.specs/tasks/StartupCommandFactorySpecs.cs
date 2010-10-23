521867b67e7fa079b3f239210bb8457001c01372
unknown <VShivaiah@.corporate.ncm>
2010-10-22 18:21:48 -0600

    web22

---
+++ product/nothinbutdotnetstore.specs/tasks/StartupCommandFactorySpecs.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Machine.Specifications;
+using Machine.Specifications.DevelopWithPassion.Rhino;
+using nothinbutdotnetstore.infrastructure.containers;
+using nothinbutdotnetstore.infrastructure.logging;
+using nothinbutdotnetstore.infrastructure.logging.log4net;
+using nothinbutdotnetstore.tasks.startup;
+using nothinbutdotnetstore.web.core;
+
+namespace nothinbutdotnetstore.specs.tasks
+{
+    class StartupCommandFactorySpecs
+    {
+        public abstract class concern : Observes<StartupCommandFactory, DefaultStartupCommandFactory>
+        {
+
+        }
+
+        [Subject(typeof(DefaultStartupCommandFactory))]
+        public class when_creating_a_startup_command_from_an_type : concern
+        {
+            private Establish c = () =>
+                                      {
+                                          dependency_type = typeof (ourStarupCommmand);
+                                          the_correct_startupCommand = an<StartupCommand>();
+                                      };
+
+            private Because b = () =>
+                                    {
+                                        result = sut.create_from(dependency_type);
+                                    };
+
+            private It should_be_able_to_create_a_startup_command_from_its_type =
+                () => result.ShouldEqual(the_correct_startupCommand);
+
+            static StartupCommand result;
+            static Type dependency_type;
+            private static StartupCommand the_correct_startupCommand;
+        }
+
+        public class ourStarupCommmand : StartupCommand
+        {
+            public void run()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
---
+++ product/nothinbutdotnetstore/tasks/startup/DefaultStartupCommandFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nothinbutdotnetstore.tasks.startup
+{
+    public class DefaultStartupCommandFactory : StartupCommandFactory
+    {
+
+        public StartupCommand create_from(Type command_type)
+        {
+            
+            //throw new NotImplementedException();
+        }
+
+    }
+}
